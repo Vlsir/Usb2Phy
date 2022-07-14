f6c10ddb9d9e7f35cff495d4d9f73a911cb6b497
Dan Fritchman <dan@fritch.mn>
2022-07-14 14:30:29 -0700

    Cmos Ilo Frequency Sweeps

--- Usb2PhyAna/usb2phyana/hsrx/ilo/ilo.py
+++ Usb2PhyAna/usb2phyana/hsrx/ilo/ilo.py
@@ -4,6 +4,8 @@
 
 # Hdl & PDK Imports
 import hdl21 as h
+from hdl21.prefix import f 
+from hdl21.primitives import C 
 
 import s130
 from s130 import MosParams 
@@ -14,8 +16,15 @@ from ...diff import Diff, inverse
 
 
 # Create the default-param'ed NMOS and PMOS
+PmosHvt = s130.modules.pmos_hvt
 Pmos = s130.modules.pmos
+Nmos = s130.modules.nmos
 NmosLvt = s130.modules.nmos_lvt
+
+
+@h.paramclass
+class IloParams:
+    cl = h.Param(dtype=h.ScalarParam, desc="Capacitance Load", default=10 * f)
 
 
 @h.generator
@@ -29,53 +38,64 @@ def IloInv(p: Width) -> h.Module:
         i = h.Input()
         o = h.Output()
         # Internal Implementation
-        nmos = NmosLvt(MosParams(m=p.width))(g=i, d=o, s=VSS, b=VSS)
-        pmos = Pmos(MosParams(m=p.width))(g=i, d=o, s=VDD, b=VDD)
+        nmos = Nmos(MosParams(m=p.width))(g=i, d=o, s=VSS, b=VSS)
+        pmos = PmosHvt(MosParams(m=p.width))(g=i, d=o, s=VDD, b=VDD)
     
     return IloInv
 
-
-@h.module
-class IloStage:
+@h.generator
+def IloStage(params: IloParams) -> h.Module:
     """ # Injection Locked Oscillator Stage 
     Note the input/ output polarities are such that the differential 
     transfer is *non* inverting. """
 
-    # IO
-    VDD, VSS = h.Ports(2)
-    inp = Diff(port=True, role=Diff.Roles.SINK)
-    out = Diff(port=True, role=Diff.Roles.SOURCE)
+    @h.module
+    class IloStage:
+        # IO
+        VDD, VSS = h.Ports(2)
+        inp = Diff(port=True, role=Diff.Roles.SINK)
+        out = Diff(port=True, role=Diff.Roles.SOURCE)
 
-    # Internal Implementation
-    fwdp = IloInv(width=8)(i=inp.p, o=out.n, VDD=VDD, VSS=VSS)
-    fwdn = IloInv(width=8)(i=inp.n, o=out.p, VDD=VDD, VSS=VSS)
-    crossp = IloInv(width=2)(i=out.p, o=out.n, VDD=VDD, VSS=VSS)
-    crossn = IloInv(width=2)(i=out.n, o=out.p, VDD=VDD, VSS=VSS)
+        # Internal Implementation
+        ## Forward Inverters 
+        fwdp = IloInv(width=16)(i=inp.p, o=out.n, VDD=VDD, VSS=VSS)
+        fwdn = IloInv(width=16)(i=inp.n, o=out.p, VDD=VDD, VSS=VSS)
+        ## Cross-Coupled Output Inverters
+        crossp = IloInv(width=4)(i=out.p, o=out.n, VDD=VDD, VSS=VSS)
+        crossn = IloInv(width=4)(i=out.n, o=out.p, VDD=VDD, VSS=VSS)
+        ## Load Caps 
+        clp = C(C.Params(c=params.cl))(p=out.p, n=VSS)
+        cln = C(C.Params(c=params.cl))(p=out.n, n=VSS)
+
+    return IloStage
 
 
-@h.module
-class Ilo:
+@h.generator
+def Ilo(params: IloParams) -> h.Module:
     """ # Injection Locked Oscillator """
 
-    # IO
-    VDD18, VSS = h.Ports(2)
-    inj = h.Input()
-    
-    # Internal Implementation
-    stg0 = Diff()
-    stg1 = Diff()
-    stg2 = Diff()
-    stg3 = Diff()
+    @h.module
+    class Ilo:
+        # IO
+        VDD18, VSS = h.Ports(2)
+        inj = h.Input()
+        
+        # Internal Implementation
+        stg0 = Diff()
+        stg1 = Diff()
+        stg2 = Diff()
+        stg3 = Diff()
 
-    ## Delay Stages
-    i0 = IloStage(inp=stg0, out=stg1, VDD=VDD18, VSS=VSS)
-    i1 = IloStage(inp=stg1, out=stg2, VDD=VDD18, VSS=VSS)
-    i2 = IloStage(inp=stg2, out=stg3, VDD=VDD18, VSS=VSS)
-    i3 = IloStage(inp=stg3, out=inverse(stg0), VDD=VDD18, VSS=VSS)
+        ## Delay Stages
+        i0 = IloStage(params)(inp=stg0, out=stg1, VDD=VDD18, VSS=VSS)
+        i1 = IloStage(params)(inp=stg1, out=stg2, VDD=VDD18, VSS=VSS)
+        i2 = IloStage(params)(inp=stg2, out=stg3, VDD=VDD18, VSS=VSS)
+        i3 = IloStage(params)(inp=stg3, out=inverse(stg0), VDD=VDD18, VSS=VSS)
 
-    ## Injection Nmos Switches
-    n0 = NmosLvt(MosParams())(g=inj, d=stg0.p, s=VSS, b=VSS)
-    n1 = NmosLvt(MosParams())(g=inj, d=stg1.p, s=VSS, b=VSS)
-    n2 = NmosLvt(MosParams())(g=inj, d=stg2.p, s=VSS, b=VSS)
-    n3 = NmosLvt(MosParams())(g=inj, d=stg3.p, s=VSS, b=VSS)
+        ## Injection Nmos Switches
+        n0 = NmosLvt(MosParams())(g=inj, d=stg0.p, s=VSS, b=VSS)
+        n1 = NmosLvt(MosParams())(g=inj, d=stg1.p, s=VSS, b=VSS)
+        n2 = NmosLvt(MosParams())(g=inj, d=stg2.p, s=VSS, b=VSS)
+        n3 = NmosLvt(MosParams())(g=inj, d=stg3.p, s=VSS, b=VSS)
 
+    return Ilo
--- Usb2PhyAna/usb2phyana/hsrx/ilo/test_ilo.py
+++ Usb2PhyAna/usb2phyana/hsrx/ilo/test_ilo.py
@@ -3,8 +3,19 @@
 # ILO Tests 
 """
 
+import pickle
+from typing import List, Tuple 
+from dataclasses import asdict
+from copy import copy 
+from pathlib import Path
+
+from pydantic.dataclasses import dataclass
+import numpy as np 
+import matplotlib.pyplot as plt 
+
 # Hdl & PDK Imports
 import hdl21 as h
+import hdl21.sim as hs 
 from hdl21.pdk import Corner
 from hdl21.sim import Sim, LogSweep
 from hdl21.prefix import m, µ, f, n, T, PICO
@@ -15,8 +26,7 @@ import sitepdks as _
 from ...tests.sim_options import sim_options
 
 # DUT Imports
-from .ilo import Ilo
-from .. import Diff
+from .ilo import Ilo, IloParams
 
 
 @h.paramclass
@@ -31,11 +41,11 @@ class Pvt:
 @h.paramclass
 class TbParams:
     pvt = h.Param(dtype=Pvt, desc="Process, Voltage, and Temperature Parameters", default=Pvt())
-    cl = h.Param(dtype=h.Prefixed, desc="Load Cap (Per Stage) (F)", default=10 * f)
+    ilo = h.Param(dtype=IloParams, desc="Ilo Generator Parameters", default=IloParams())
 
 
 @h.generator
-def IloTb(p: TbParams) -> h.Module:
+def IloInjectionTb(params: TbParams) -> h.Module:
     """ Ilo Testbench """
 
     # Create our testbench 
@@ -43,7 +53,7 @@ def IloTb(p: TbParams) -> h.Module:
 
     # Generate and drive VDD
     tb.VDD = VDD = h.Signal()
-    tb.vvdd = Vdc(Vdc.Params(dc=p.pvt.v))(p=VDD, n=tb.VSS)
+    tb.vvdd = Vdc(Vdc.Params(dc=params.pvt.v))(p=VDD, n=tb.VSS)
 
     # Create the injection-pulse source, 
     # which also serves as our kick-start
@@ -59,7 +69,7 @@ def IloTb(p: TbParams) -> h.Module:
         )(p=tb.inj, n=tb.VSS)
 
     # Create the Ilo DUT 
-    tb.dut = Ilo(
+    tb.dut = Ilo(params.ilo)(
          inj=tb.inj, VDD18=VDD, VSS=tb.VSS,
     )
     return tb
@@ -69,8 +79,8 @@ def test_ilo_sim():
     """ Ilo Test(s) """
 
     # Create our parametric testbench 
-    params = TbParams(pvt=Pvt(v=900*m), cl = 10*f)
-    tb = IloTb(params)
+    params = TbParams(pvt=Pvt(v=900*m), cl = 30*f)
+    tb = IloInjectionTb(params)
 
     # And simulation input for it 
     sim = Sim(tb=tb, attrs=s130.install.include(Corner.TYP))
@@ -79,3 +89,188 @@ def test_ilo_sim():
     # Run some spice
     results = sim.run(sim_options)
     print(results)
+
+
+@h.generator
+def IloFreqTb(params: TbParams) -> h.Module:
+    """ Ilo Frequency Testbench """
+
+    # Create our testbench 
+    tb = h.sim.tb("IloTb")
+
+    # Generate and drive VDD
+    tb.VDD = VDD = h.Signal()
+    tb.vvdd = Vdc(Vdc.Params(dc=params.pvt.v))(p=VDD, n=tb.VSS)
+
+    # Create the injection-pulse source, which in this bench serves entirely as a kick-start
+    tb.inj = h.Signal()
+    tb.vinj = Vpulse(Vpulse.Params(
+            v1=1800*m,
+            v2=0,
+            period=1001, # "Infinite" period
+            rise=10 * PICO,
+            fall=10 * PICO,
+            width=1000,
+            delay=10 * PICO)
+        )(p=tb.inj, n=tb.VSS)
+
+    # Create the Ilo DUT 
+    tb.dut = Ilo(params.ilo)(
+         inj=tb.inj, VDD18=VDD, VSS=tb.VSS,
+    )
+    return tb
+
+
+@dataclass 
+class Result:
+    """ Result of the (V,F) Sweep, Parameterized across Process and Temp """
+
+    # Process, Temperature Conditions 
+    conditions: List[Tuple[Corner, int]]
+    # Vdds per Process, Temperature Condition
+    vdds: List[h.Prefixed]
+    # Sim Results, per Vdd Point 
+    results: List[List[hs.SimResult]]
+
+
+def run_corners(tbgen: h.Generator) -> Result:
+    """ Run `sim` on `tbgen`, across corners """
+
+    opts = copy(sim_options)
+    opts.rundir = None
+
+    # (P,T) Conditions 
+    conditions = [{
+            "p": corner,
+            "t": temper,
+        }
+        for corner in [Corner.TYP, Corner.FAST, Corner.SLOW]
+        for temper in [-25, 25, 75]
+    ]
+
+    # Initialize our results
+    result = Result(
+        conditions=[(cond["p"], cond["t"]) for cond in conditions], 
+        vdds = [val*m for val in range(200, 2100, 100)],
+        results=[]
+    )
+
+    # Run conditions one at a time, parallelizing across PI codes
+    for cond in conditions:
+        print(f"Simulating (vdd, freq) for {cond}")
+
+        # Create a list of per-voltage tb parameters
+        params = [TbParams(
+            pvt=Pvt(v=v, **cond), 
+            ilo=IloParams(cl=30*f)
+        ) for v in result.vdds]
+        
+        # Create the simulation inputs
+        sims = [sim_input(tbgen=tbgen, params=p) for p in params]
+
+        # Run sims
+        condition_results = h.sim.run(sims, opts)
+        result.results.append(condition_results)
+
+    
+    pickle.dump(asdict(result), open("result.pkl", "wb"))
+
+    return result
+
+def plot(result: Result, title: str, fname: str):
+
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+    vdds = np.array([1000 * float(v) for v in result.vdds])
+
+    for (cond, cond_results) in zip(result.conditions, result.results):
+        # Post-process the results into (vdd, period) curves
+        freqs = np.array([1 / tperiod(r) for r in cond_results])
+        idds = np.array([idd(r) for r in cond_results])
+
+        # Numpy interpolation requires the x-axis array be NaN-free
+        # This often happens at low Vdd, when the ring fails to oscillate, 
+        # or goes slower than we care to simulate. 
+        # Replace any such NaN values with zero. 
+        # If there are any later in the array, this interpolation will fail. 
+        freqs_no_nan = np.nan_to_num(freqs, copy=True, nan=0)
+        idd_480 = np.interp(x=480e6, xp=freqs_no_nan, fp=idds)
+        vdd_480 = np.interp(x=480e6, xp=freqs_no_nan, fp=vdds)
+        print(cond, vdd_480, idd_480)
+
+        # And plot the results
+        label = str(cond) ##f"{cond[0], cond[1]}" ## FIXME! f"{params.pvt.corner} {params.pvt.temper}"
+        ax.plot(freqs / 1e9, np.abs(idds), label=label)
+        ax2.plot(freqs / 1e9, vdds)
+    
+    # Set up all the other data on our plot
+    ax.set_title(title)
+    ax.set_xlabel("Freq (GHz)")
+    ax.set_ylabel("Idd (A)")
+    ax2.set_ylabel("Vdd (mV)")
+    ax.legend()
+
+    # And save it to file
+    fig.savefig(fname)
+
+
+def idd(results: "SimResult") -> float:
+    return results.an[0].measurements["idd"]
+
+def tperiod(results: "SimResult") -> float:
+    return results.an[0].measurements["tperiod"]
+
+
+def sim_input(tbgen: h.Generator, params: TbParams) -> hs.Sim:
+    """ Ilo Frequency Sim """
+
+    print(f"Simulating Ilo for voltage {params.pvt.v}")
+
+    # Create some simulation stimulus
+    @hs.sim
+    class IloSim:
+        # The testbench 
+        tb = tbgen(params)
+
+        # Our sole analysis: transient, for much longer than we need. 
+        # But auto-stopping when measurements complete. 
+        tr = hs.Tran(tstop=500 * n)
+
+        # Measurements
+        trise5 = hs.Meas(tr, expr="when 'V(xtop.dut.stg0_p)-V(xtop.dut.stg0_n)'=0 rise=5")
+        trise15 = hs.Meas(tr, expr="when 'V(xtop.dut.stg0_p)-V(xtop.dut.stg0_n)'=0 rise=15")
+        tperiod = hs.Meas(tr, expr="param='(trise15-trise5)/10'")
+        idd = hs.Meas(tr, expr="avg I(xtop.vvdd) from=trise5 to=trise15")
+
+        # The stuff we can't first-class represent, and need to stick in a literal. 
+        _ = hs.Literal(
+            f"""
+            simulator lang=spice
+            .temp {params.pvt.t}
+            .option autostop
+            simulator lang=spectre
+        """) 
+
+    # Add the PDK dependencies 
+    IloSim.add(*s130.install.include(params.pvt.p))
+
+    # # FIXME: handling of multi-directory sims
+    # opts = copy(sim_options)
+    # opts.rundir = Path(f"./scratch/")
+    
+    return IloSim
+
+
+def test_ilo_freq():
+    """ Ilo Frequence Test(s) """
+
+    # Run corner simulations to get results 
+    result = run_corners(IloFreqTb)
+
+    # Or just read them back from file, if we have one
+    # result = Result(**pickle.load(open("result.pkl", "rb")))
+
+    # And make some pretty pictures
+    plot(result, "IloFreq", "IloFreq.png")
+
+
